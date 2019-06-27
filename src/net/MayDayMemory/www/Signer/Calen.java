7936f390b13020d853dcf4b9f035bdde08f6ee7d
魏宇强 <286799714@qq.com>
2019-06-27 19:44:03 +0800

    签到板主程序

--- src/net/MayDayMemory/www/Signer/Calen.java
+++ src/net/MayDayMemory/www/Signer/Calen.java
@@ -2,14 +2,12 @@ package net.MayDayMemory.www.Signer;
 
 import java.util.Calendar;
 
-import org.bukkit.Material;
-
 
 public class Calen implements java.io.Serializable{
-	public Material[] sticky;
+	public String[] state;
 	public int lastm;
 	public int lasty;
-	public boolean state;  //true 闰年  false则不是
+	public boolean fixstate;  //true 是闰年 
 	private static final long serialVersionUID = -1458510272181181143L;
 	
 	public Calen(Calendar c){
@@ -17,52 +15,51 @@ public class Calen implements java.io.Serializable{
 		if(year%4==0){
 			if(year%100==0){
 				if(year%400==0){
-					state = true;
+					fixstate = true;
 				}
-				state = false;
+				fixstate = false;
 			}
-			state = true;
+			fixstate = true;
 		}
-		int month = c.get(Calendar.MONTH);
+		int month = c.get(Calendar.MONTH)+1;
 		int day = c.get(Calendar.DATE);
 		
 		if(month == 2){
-			if(state==true){
-				sticky = new Material[28];
+			if(fixstate==true){
+				state = new String[28];
 			}else{
-				sticky = new Material[29];
+				state = new String[29];
 			}
 		}else if(month == 1){
-			sticky = new Material[31];
+			state = new String[31];
 		}else if(month<8){
 			if(month%2==0){
-				sticky = new Material[30];
+				state = new String[30];
 			}else{
-				sticky= new Material[31];
+				state= new String[31];
 			}
 		}else{
 			if(month%2==0){
-				sticky= new Material[31];
+				state= new String[31];
 			}else{
-				sticky = new Material[30];
+				state= new String[30];
 			}
 		}
 		
 		int f;
 		for(f = 0;f<day;f++){
-			sticky[f]=Material.ICE;
+			state[f]="unfinished";
 		}
-		for(int f2=f;f2<sticky.length;f2++){
-			sticky[f2]=Material.SNOW_BLOCK;
+		for(int f2=f;f2<state.length;f2++){
+			state[f2]="comming";
 		}
-		
 		lastm = c.get(Calendar.MONTH);
 		lasty = c.get(Calendar.YEAR);
 	}
 	
 	public void update(){
 		int date = Calendar.getInstance().get(Calendar.DATE);
-		sticky[date-1] = Material.EMERALD_BLOCK;
+		state[date-1] ="finished";
 		lastm = Calendar.getInstance().get(Calendar.MONTH);
 		lasty = Calendar.getInstance().get(Calendar.YEAR);
 	}
