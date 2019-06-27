97a5f641cfdf656b012e26190e99595a468f9f9b
魏宇强 <286799714@qq.com>
2019-06-27 19:43:39 +0800

    修复了1.7.10的bug

--- src/net/MayDayMemory/www/Events/InventoryClick.java
+++ src/net/MayDayMemory/www/Events/InventoryClick.java
@@ -7,7 +7,6 @@ import net.MayDayMemory.www.Main;
 
 import org.bukkit.Effect;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class InventoryClick implements Listener{
 	@EventHandler
@@ -25,9 +25,16 @@ public class InventoryClick implements Listener{
 		int month = Calendar.getInstance().get(Calendar.MONTH)+1;
 		if (title.equals("§0§l"+month+"月签到簿")){
 			if(e.getSlot()+1==Calendar.getInstance().get(Calendar.DATE)){
-				if(!i.getItem(e.getSlot()).equals( new ItemStack(Material.EMERALD_BLOCK,e.getSlot()+1))){
-					i.setItem(e.getSlot(), new ItemStack(Material.EMERALD_BLOCK,e.getSlot()+1));
-					Main.instance.cmap.get(p.get(0).getUniqueId()).sticky[e.getSlot()]=Material.EMERALD_BLOCK;
+				int slot = e.getSlot();
+				ItemStack finished = Main.instance.getConfig().getItemStack("symbol.finished.mate");
+				String name =Main.instance.getConfig().getString("symbol.finished.displayname").replaceAll("0day0",String.valueOf(slot+1));
+				finished.setAmount(slot+1);
+				ItemMeta newmeta =finished.getItemMeta();
+				newmeta.setDisplayName(name);
+				finished.setItemMeta(newmeta);
+				if(!i.getItem(slot).equals(finished)){
+					i.setItem(slot, finished);
+					Main.instance.cmap.get(p.get(0).getUniqueId()).state[slot]="finished";
 					Main.instance.cser((Player)p.get(0));
 					for(String m:Main.instance.getConfig().getStringList("commands"))   ((Player) p.get(0)).chat(m);
 					if(Main.instance.getConfig().contains("items")){
@@ -37,7 +44,7 @@ public class InventoryClick implements Listener{
 						}
 					}
 					Location location = p.get(0).getLocation();
-					location.getWorld().playEffect(location, Effect.ANVIL_USE, 1);
+					location.getWorld().playEffect(location, Effect.BOW_FIRE, 1);
 					for (int degree = 0; degree < 360; degree++) {
 					    double radians = Math.toRadians(degree);
 					    double x = Math.cos(radians);
